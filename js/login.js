2cacc1de0106ff489d25664c703bdd6eed0509b5
lucidlactose <jesusvillarruel98@gmail.com>
2020-09-05 14:08:00 -0700

    added the ability to add cookies without a backend server

--- js/login.js
+++ js/login.js
@@ -13,13 +13,29 @@ $(window).on("load", function() {
             authenticateUser: function(email,password){
 				// if there's a successful login, save the user into a cookie and from the admin page, check
 				// if there exists a user in the cookie
-				//firebase.auth().createUserWithEmailAndPassword(email, password)
                 firebase
 					.auth()
 					.signInWithEmailAndPassword(email, password)
 					.then(function(user) {
 						// this is where I authorize
-						window.location.href = 'admin.html';
+						// window.location.href = 'admin.html';
+						$cookies.set("SOMTHING", "TESTING NUMBER OISOFUHE", 1)
+
+						// **********************************************
+						// Theoretically, this should get a session from the backend and redirect
+						// based on a valid session, but no user is ever found nor is a session
+						// cookie returned without a server
+						// **********************************************
+						// admin.auth().createSessionCookie(idToken, {expiresIn})
+						// .then((sessionCookie) => {
+						// 	// Set cookie policy for session cookie.
+						// 	const options = {maxAge: expiresIn, httpOnly: true, secure: true};
+						// 	res.cookie('session', sessionCookie, options);
+						// 	res.end(JSON.stringify({status: 'success'}));
+						// }, error => {
+						// 	res.status(401).send('UNAUTHORIZED REQUEST!');
+						// });
+
 					})
 					.catch(function(error) {
 						var errorCode = error.code;
--- js/main.js
+++ js/main.js
@@ -4,15 +4,52 @@ $(window).on("load", function() {
 	
 	// Your web app's Firebase configuration
 	var firebaseConfig = {
-		apiKey: "AIzaSyAVecyxSiOL7L99QhX2pVoSMST2k7O1IBI",
-		authDomain: "website-on-dashnex.firebaseapp.com",
-		databaseURL: "https://website-on-dashnex.firebaseio.com",
-		projectId: "website-on-dashnex",
-		storageBucket: "website-on-dashnex.appspot.com",
-		messagingSenderId: "4818761514",
-		appId: "1:4818761514:web:d7c36c56d983b4676c5053",
-		measurementId: "G-66NVZDRVXM"
-	  };
+		apiKey: "AIzaSyAqs-0W9j6q4vjbXAMh_uKObfTGmHM3SSo",
+		authDomain: "test-b9dc7.firebaseapp.com",
+		databaseURL: "https://test-b9dc7.firebaseio.com",
+		projectId: "test-b9dc7",
+		storageBucket: "test-b9dc7.appspot.com",
+		messagingSenderId: "322217195913",
+		appId: "1:322217195913:web:575914c293a3a71639c958",
+		measurementId: "G-NJDBXM4XB7"
+	};
+
+	// var firebaseConfig = {
+	// 	apiKey: "AIzaSyAVecyxSiOL7L99QhX2pVoSMST2k7O1IBI",
+	// 	authDomain: "website-on-dashnex.firebaseapp.com",
+	// 	databaseURL: "https://website-on-dashnex.firebaseio.com",
+	// 	projectId: "website-on-dashnex",
+	// 	storageBucket: "website-on-dashnex.appspot.com",
+	// 	messagingSenderId: "4818761514",
+	// 	appId: "1:4818761514:web:d7c36c56d983b4676c5053",
+	// 	measurementId: "G-66NVZDRVXM"
+	//   };
 	// Initialize Firebase
 	firebase.initializeApp(firebaseConfig);
+
+
+
+	/// ****************************************
+	// All this below allow to access cookies but a user object is nonexistent
+	/// ****************************************
+	// As httpOnly cookies are to be used, do not persist any state client side.
+	// firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
+
+
+	// firebase.auth().signInWithEmailAndPassword('batmanandrobin255@gmail.com', 'somethingelse').then(({ user }) => {
+	// 	// Get the user's ID token as it is needed to exchange for a session cookie.
+	// 	return user.getIdToken().then(idToken => {
+	// 	  // Session login endpoint is queried and the session cookie is set.
+	// 	  // CSRF protection should be taken into account.
+	// 	  // ...
+	// 	  const csrfToken = getCookie('csrfToken')
+	// 	  return postIdTokenToSessionLogin('/sessionLogin', idToken, csrfToken);
+	// 	})
+	// }).then(() => {
+	// 	// A page redirect would suffice as the persistence is set to NONE.
+	// 	return firebase.auth().signOut();
+	// }).then(() => {
+	// 	window.location.assign('/profile');
+	// });
+
 });
